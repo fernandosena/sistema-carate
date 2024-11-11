7f3227145f22570c17e0e1d6ff6729e3fd0a41b6
Fernando <fernandocarvalho.sena@gmail.com>
2024-11-11 14:39:52 -0300

    perf: Removido links para acessar admin e app

--- themes/adminlte/_admin.php
+++ themes/adminlte/_admin.php
@@ -225,12 +225,6 @@
                                 ],
                             );
                             echo '<li class="nav-header">Sistema</li>';
-                            echo "<li class='nav-item'>
-                                    <a href='".url("app")."' class='nav-link'>
-                                        <i class='fa-duotone fa-solid fa-sidebar-flip'></i>
-                                        <p>App</p>
-                                    </a>
-                                </li>";
                             echo $nav(
                                 [
                                     "icon"=>"fa-sign-out",
