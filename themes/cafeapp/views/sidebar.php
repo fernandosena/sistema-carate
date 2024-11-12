aa7477f768f1f606acbef98becb893f449696463
Fernando <fernandocarvalho.sena@gmail.com>
2024-11-12 17:36:28 -0300

    fix: :bug: Resolvido bug de certificado no app

--- themes/cafeapp/views/sidebar.php
+++ themes/cafeapp/views/sidebar.php
@@ -1,6 +1,6 @@
 <div class="app_sidebar_nav">
     <a class="icon-home radius transition" title="Dashboard" href="<?= url("/app"); ?>">Controle</a>
-    <a class="icon-bookmark-o radius transition" target="__blank" title="Certificado" href="<?= url("certificado/".user()->document."/certificado"); ?>">Meu Certificado</a>
+    <a class="icon-bookmark-o radius transition" target="__blank" title="Certificado" href="<?= url("certificado/".user()->document."/certificado/".user()->id); ?>">Meu Certificado</a>
     <a class="icon-user radius transition" title="Alunos Dan" href="<?= url("/app/alunos/black"); ?>">Alunos Dan</a>
     <a class="icon-user radius transition" title="Alunos Kyus" href="<?= url("/app/alunos/kyus"); ?>">Alunos Kyus</a>
     <a class="icon-user radius transition" title="Perfil" href="<?= url("/app/perfil"); ?>">Perfil</a>
