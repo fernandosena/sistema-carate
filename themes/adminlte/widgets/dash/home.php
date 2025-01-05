7883f8c6eb723fe6280089e31a68aaf49c5e6349
Fernando <fernandocarvalho.sena@gmail.com>
2025-01-05 11:49:33 -0300

    style: :art: Alterado texto e card

--- themes/adminlte/widgets/dash/home.php
+++ themes/adminlte/widgets/dash/home.php
@@ -62,7 +62,7 @@
         <!-- BAR CHART -->
         <div class="card card-success">
             <div class="card-header">
-                <h3 class="card-title">Quantidade de alunos/instrutores - <?= date("Y") ?></h3>
+                <h3 class="card-title">Quantidade de alunos/instrutores</h3>
             </div>
             <div class="card-body">
                 <div class="row">
--- themes/adminlte/widgets/instructors/instructor.php
+++ themes/adminlte/widgets/instructors/instructor.php
@@ -62,7 +62,7 @@
     <!-- BAR CHART -->
     <div class="card card-success">
             <div class="card-header">
-                <h3 class="card-title">Quantidade de alunos - <?= date("Y") ?></h3>
+                <h3 class="card-title">Quantidade de alunos</h3>
             </div>
             <div class="card-body">
                 <div class="row">
